b587ac68e28f399cb15fab639fac9379b71d8c44
RutujaWanjari <rutuja.wanjari2@gmail.com>
2017-05-21 14:55:01 +0530

    palindrome string

--- palindrome_string.py
+++ palindrome_string.py
@@ -7,6 +7,7 @@ my_str = sys.stdin.readline()
 # remove newline character-"\n" from string.
 my_str = my_str.rstrip()
 
+# *************** ONE WAY *****************
 # make it suitable for caseless comparison
 my_str = my_str.casefold()
 
@@ -15,6 +16,13 @@ rev_str = reversed(my_str)
 
 # check if the string is equal to its reverse
 if list(my_str) == list(rev_str):
+    print("It is palindrome")
+else:
+    print("It is not palindrome")
+
+# ************** ANOTHER WAY ***************
+# the [::-1] slice inverts the string
+if str(my_str) == str(my_str)[::-1]:
     print("It is palindrome")
 else:
     print("It is not palindrome")
